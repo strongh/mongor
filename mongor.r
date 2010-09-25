6ecfe92b4616cf79dae63c979f96bba22f51253a
Homer Strong <homer.strong@gmail.com>
2010-09-25 14:17:42 -0700

    querying works!

--- mongor.r
+++ mongor.r
@@ -22,14 +22,17 @@ op_msg = function(){
            ))
 }
 
-op_query = function(){
-  ## header
-##  fut_use = numToRaw(0, nBytes = 4) # reserved for future use
+op_query =
+  function(
+           collection='test.$cmd',
+           query_list=list(count='foo', query=list(), fields=NULL))
+{
+    ## header
   flags = op_query_flags()
-  full_name = encode_cstring('test.$cmd') # full collection name
+  full_name = encode_cstring(collection) # full collection name
   to_skip = numToRaw(0, nBytes = 4)  # number of documents to skip
-  to_return = numToRaw(1, nBytes = 4) #number of entries to return
-  query = encode_document(list(count='foo', query=list(), fields=NULL)) # empty query
+  to_return = numToRaw(10, nBytes = 4) #number of entries to return
+  query = encode_document(query_list) # empty query
   
   rawl = c(flags, full_name, to_skip, to_return, query)
 
@@ -37,11 +40,12 @@ op_query = function(){
   return(c(header, rawl))
 }
 
-op_insert = function(){
+
+op_insert = function(doc){
   ## header is sent first, but is added last
   fut_use = numToRaw(0, nBytes = 4) # reserved for future use
   full_name = encode_cstring('test.foo') # full collection name
-  to_insert = encode_document(list(thurs='hungry'))
+  to_insert = encode_document(doc)
   
   rawl = c(fut_use, full_name, to_insert)
 
@@ -57,7 +61,7 @@ mongo_send = function(msg){
 
 
 mongo_get = function(){
-#  osock = socketConnection(port=27017, open='wb')
+#  sock = socketConnection(port=27017, open='wb')
   str = ''
   repeat{
     nxt = suppressWarnings(rawToChar(readBin(con=sock, what='raw')))
@@ -70,7 +74,9 @@ mongo_get = function(){
   return(str)
 }
 
+
 mongo_reply = function(){
+
   msg.size = decode_int32(readBin(con=sock, what='raw', n=4)) # first int32, size of message
   msg = readBin(con=sock, what='raw', n=(msg.size-4))
 
@@ -99,7 +105,44 @@ mongo_reply = function(){
   num.returned = msg[1:4]
   msg = msg[-c(1:4)]
 
-  out = decode_document(msg)
+  out = list()
 
+  while(length(msg) > 4){ # a document is at least 5 bytes
+    next.doc.len = decode_int32(msg[1:4])
+    doc = decode_document(msg[1:next.doc.len])
+
+    doc = doc[-pmatch("_id", names(doc))]
+    out = append(out, doc)    
+    msg = msg[-c(1:next.doc.len)]
+  }
   out
 }
+
+
+mongoCount = function(attempts = 10){
+  mongo_send(op_query())
+  for(i in 1:attempts){
+    tt = try(mongo_reply(), silent=TRUE)
+    if(class(tt) != "try-error")
+      break()
+  }
+  tt$n
+}
+
+mongoFindOne = function(attempts = 10){
+  mongo_send(op_query(collection='test.foo', query=list(`$query`=list(hello="world"))))
+  for(i in 1:attempts){
+    tt = try(mongo_reply(), silent=TRUE)
+    if(class(tt) != "try-error")
+      break()
+  }
+  tt
+}
+
+
+mongoInsert = function(doc){
+  mongo_send(op_insert(doc))
+  mongoCount()
+}
+
+
