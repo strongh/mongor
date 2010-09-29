dc60e054bb52cc3d21ee3c53b89c0fd571376e72
Homer Strong <homer.strong@gmail.com>
2010-09-29 00:11:21 -0700

    added to examples

--- ex.R
+++ ex.R
@@ -2,3 +2,7 @@ library(mongor)
 
 mc = mongoConnect()
 mongoQuery(mc, list(hello='world'))
+mongoQuery(mc, list(good='evening'))
+mongoQuery(mc, list(better='evening'))
+mongoInsert(mc, list(new='hotshit'))
+mongoQuery(mc, list(new='hotshit'))
