f0320082803a7f92452fdb29b85c81bd181f5ccb
Homer Strong <homer.strong@gmail.com>
2010-09-29 21:33:00 -0700

    updated example and increased max

--- R/op_query.R
+++ R/op_query.R
@@ -13,7 +13,7 @@ op_query <-
     flags = op_query_flags()
     full_name = encode_cstring(collection) # full collection name
     to_skip = numToRaw(0, nBytes = 4)  # number of documents to skip
-    to_return = numToRaw(10, nBytes = 4) #number of entries to return
+    to_return = numToRaw(200, nBytes = 4) #number of entries to return
     query = encode_document(query_list) # empty query
     
     rawl = c(flags, full_name, to_skip, to_return, query)
