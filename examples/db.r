9717f7f0d43823b29fa39da504eb86f14976b923
Homer Strong <homer.strong@gmail.com>
2010-11-14 11:16:06 -0800

    added example file for mongoDb objects

---
+++ examples/db.r
@@ -0,0 +1,9 @@
+library(mongor) # load mongor, etc.
+
+## get a mongoDb object.
+md = mongoDb("test")
+
+## we can query on collections from this database,
+## without re-specifiying the connection or databse
+mongoFind(md, "foo", list(hello="world")
+mongoFind(md, "foo", list(apples=list("$gt"=3))) # where apples are greater than 3
