c4a9322998bb1e570172ba37d5269538a3ca5b66
Homer Strong <homer.strong@gmail.com>
2010-11-14 11:05:48 -0800

    updated connection examples

--- examples/connections.r
+++ examples/connections.r
@@ -1,23 +1,27 @@
-##' mongor example script
-##' this is meant to give you a brief idea of how mongor can be used right now
+## mongor example script
+## this is meant to give you a brief idea of how mongor can be used right now
 
 library(mongor) # load mongor, etc.
 
 mc <- mongoConnect() # make a connection
-## by default, these operations are on the 'test' db, collection 'foo'
-##
 ## since MongoDB is document-based, you can insert lists with many different types
 
-## list databases on this host
-listDb(mc)
+## How to query and insert documents to a collection 
+mongoInsert(mc,
+            "test",
+            "foo",
+            list(hello="world",
+                 apples=3,
+                 time=Sys.time())) # insert a document
 
-##' How to query and insert documents to a collection 
-mongoInsert(mc, list(hello="world", apples=3, time=Sys.time())) # insert a document
-mongoInsert(mc, list(hello="world", apples=89, time=Sys.time())) # insert a document
-mongoInsert(mc, list(hello="earth", apples=91, time=Sys.time())) # insert a document
-mongoInsert(mc, list(hello="earth", datums=2:20))
+mongoInsert(mc,
+            "test",
+            "foo",
+            list(hello="world", apples=89, time=Sys.time())) # insert a document
+mongoInsert(mc, "test", "foo", list(hello="earth", apples=91, time=Sys.time())) # insert a document
+mongoInsert(mc, "test", "foo", list(hello="earth", datums=2:20))
 
-mongoInsert(mc, list(hello="mongo", bananas=5, hungry=TRUE)) # insert another document
+mongoInsert(mc, "test", "foo", list(hello="mongo", bananas=5, hungry=TRUE)) # insert another document
 
 ## here are some examples of using mongo's document-based queries
 ## queries for documents for which hello = 'world'
@@ -36,3 +40,27 @@ mongoFind(mc, "test", "foo",
 mongoFind(mc, "test", "foo", list(bananas=5)) # it's there!
 mongoDelete(mc, "test", "foo", list(bananas=5))
 mongoFind(mc, "test", "foo", list(bananas=5)) # it's not there!
+
+## list databases on this host
+listDb(mc)
+## insert into a new database; mongo inits it for you
+mongoInsert(mc, # insert another document...
+            "endor", # into a database which doesn't yet exist!
+            "foo",
+            list(hello="mongo", bananas=5, hungry=TRUE))
+## now look at everything in this new DB
+mongoFind(mc, "endor", "foo", list())
+## that's enough. let's drop this DB.
+dropDb(mc, "endor")
+## but is it _really_ gone?
+listDb(mc)
+## yes!
+
+
+## commands are MongoDB operations. they include methods for
+## maintenance, optimization, modes, and much more.
+## for example, 
+mongoRunCommand(mc, list(ping=1))
+## mongoRunCommand will execute the mongo command and return the result.
+## some commands must be run on the `admin' DB, while others can be run on
+## any DB.
