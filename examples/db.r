7f1a95cb742e8163151f895f765be430a67dbe01
Homer Strong <homer.strong@gmail.com>
2010-11-14 17:42:36 -0800

    fixed db example typo

--- examples/db.r
+++ examples/db.r
@@ -5,5 +5,5 @@ md = mongoDb("test")
 
 ## we can query on collections from this database,
 ## without re-specifiying the connection or databse
-mongoFind(md, "foo", list(hello="world")
+mongoFind(md, "foo", list(hello="world"))
 mongoFind(md, "foo", list(apples=list("$gt"=3))) # where apples are greater than 3
